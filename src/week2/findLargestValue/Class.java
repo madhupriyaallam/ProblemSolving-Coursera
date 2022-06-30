22c64ed87530fc5f264e27074e507f0c2d677189
madhupriyaallam <madhupriya.allam@zemosolabs.com>
2022-06-30 12:31:23 +0530

    Finding largest number

---
+++ src/week2/findLargestValue/Class.java
@@ -0,0 +1,17 @@
+package week2.findLargestValue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Class {
+    public static void main(String[] args) {
+        List<Integer> marks = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 5; i++){
+            marks.add(sc.nextInt());
+        }
+        HighestScore topScore = new Test1(marks);
+        System.out.println("The highest score is : "+topScore.getTopScore());
+    }
+}
